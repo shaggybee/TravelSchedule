fc2993bf6cf8d4890bd04d43a1117cf7855f099a
Vadim Kislov <kislov.v@gmail.com>
2026-08-07 12:41:14 +0300

    [sprint_20] replace user agreements

--- TravelSchedule/Features/UserAgreement/ViewModels/UserAgreementViewModel.swift
+++ TravelSchedule/Features/UserAgreement/ViewModels/UserAgreementViewModel.swift
@@ -16,7 +16,7 @@ final class UserAgreementViewModel {
         
         contentController.addUserScript(
             WKUserScript(
-                source: getScriptForUpdateAgreementPageTheme(),
+                source: getScriptForUpdateAgreementPage(),
                 injectionTime: .atDocumentEnd,
                 forMainFrameOnly: true
             )
@@ -30,37 +30,40 @@ final class UserAgreementViewModel {
     
     
     // MARK: - Private methods
-    private func getScriptForUpdateAgreementPageTheme() -> String {
+    private func getScriptForUpdateAgreementPage() -> String {
         let backgroundColor = getColorHexForTheme(color: .ypWhite)
         let textColor = getColorHexForTheme(color: .ypBlack)
         
+        // На странице находим нужные блоки для вывода: заголовок (dc-doc-page-title) и сам контент (dc-doc-page__body).
+        // Формируем из них тело html страницы. Заменяем стили на кастомные через document.head.innerHTML.
+        // При изменении Яндексом верстки, скрипт может работать не корректно, так как привязываемся к классам для поиска соответствующих блоков.
         let script = """
             (() => {
                 const header = document.querySelector(".\(Constants.pageTitleClass)");
                 const content = document.querySelector(".\(Constants.pageBodyClass)");
         
-            document.head.innerHTML = `
-                <style>
-                    body {
-                        font-family: -apple-system;
-                        color: \(textColor);
-                        background-color: \(backgroundColor);
-                        padding: 0 4px;
-                    }
-        
-                    h1, h2 {
-                        font-size: 24px;
-                    }
-        
-                    li {
-                        display: list-item;
-                    }
-        
-                    div {
-                        overflow: hidden;
-                    }
-                </style>
-            `;
+                document.head.innerHTML = `
+                    <style>
+                        body {
+                            font-family: -apple-system;
+                            color: \(textColor);
+                            background-color: \(backgroundColor);
+                            padding: 0 4px;
+                        }
+            
+                        h1, h2 {
+                            font-size: 24px;
+                        }
+            
+                        li {
+                            display: list-item;
+                        }
+            
+                        div {
+                            overflow: hidden;
+                        }
+                    </style>
+                `;
         
                 document.body.innerHTML = `${header?.outerHTML ?? ""} ${content?.outerHTML ?? ""}`;
             })();
